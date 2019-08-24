91c5b5071226dbb63b765f69a00a879d48c60fbd
Alfred <alfred@ningzhiyingdeiMac.local>
2019-08-24 21:59:32 +0800

    add share txt

--- invitation/weixin.js
+++ invitation/weixin.js
@@ -2,7 +2,7 @@
 wx.config({
     // 配置信息, 即使不正确也能使用 wx.ready
     debug: false,
-    appId: '',
+    appId: 'wx1f9dc3bb244623df',
     timestamp: 1,
     nonceStr: '',
     signature: '',
@@ -21,10 +21,10 @@ wx.ready(function() {
 // 分享给朋友
 wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
     wx.updateAppMessageShareData({ 
-        title: '赵峰&王一雪婚礼请柬', // 分享标题
-        desc: '邀请您参加赵峰和王一雪的婚礼，10月27日', // 分享描述
-        link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-        imgUrl: '', // 分享图标
+        title: '赵峰&王一雪婚礼邀请函', // 分享标题
+        desc: '邀请您和家人于10月27日，在北京饭店诺金，参加赵峰和王一雪的婚礼', // 分享描述
+        link: 'https://weixin.youheng-inc.com/invitation/index.html?src=friend', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: 'https://weixin.youheng-inc.com/invitation/images/o_1c53742l51g721o5d12lb1d5812dvqe.jpg', // 分享图标
         success: function () {
           // 设置成功
         }
@@ -34,9 +34,10 @@ wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
 // 分享到朋友圈
 wx.ready(function () {      //需在用户可能点击分享按钮前就先调用
     wx.updateTimelineShareData({ 
-        title: '', // 分享标题
-        link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-        imgUrl: '', // 分享图标
+        title: '赵峰&王一雪婚礼邀请函', // 分享标题
+        desc: '邀请您和家人于10月27日，在北京饭店诺金，参加赵峰和王一雪的婚礼', // 分享描述
+        link: 'https://weixin.youheng-inc.com/invitation/index.html?src=circle', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: 'https://weixin.youheng-inc.com/invitation/images/o_1c53742l51g721o5d12lb1d5812dvqe.jpg', // 分享图标
         success: function () {
           // 设置成功
         }
