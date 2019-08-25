4ac30c9863d967911e4efda98df42e3881871b70
Alfred <alfred@ningzhiyingdeiMac.local>
2019-08-25 09:55:06 +0800

    up js sdk

--- invitation/weixin.js
+++ invitation/weixin.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         var config = res.data
         wx.config({
         // 配置信息, 即使不正确也能使用 wx.ready
-        debug: false,
+        debug: true,
         appId: config.appId,
         nonceStr: config.nonceStr,
         signature: config.signature,
