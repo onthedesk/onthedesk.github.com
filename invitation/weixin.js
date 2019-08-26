3882aa0ade86fbb71988febed0809f86d88e94f7
Feng Zhao <zhaofeng@meican-inc.com>
2019-08-26 10:51:32 +0800

    add mini program nav

--- invitation/weixin.js
+++ invitation/weixin.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
       wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
           wx.updateAppMessageShareData({ 
               title: '赵峰&王一雪婚礼请柬', // 分享标题
-              desc: '邀请您和家人于10月27日，在北京饭店诺金参加赵峰和王一雪的婚礼，恭候您的光临。', // 分享描述
+              desc: '邀请您和家人于10月27日光临北京饭店参加赵峰和王一雪的婚礼。', // 分享描述
               link: 'https://weixin.youheng-inc.com/invitation/index.html?src=friend', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
               imgUrl: 'https://invitation.youheng-inc.com/images/o_1c53742l51g721o5d12lb1d5812dvqe.jpg', // 分享图标
               success: function () {
@@ -38,7 +38,7 @@ $(document).ready(function() {
       wx.ready(function () {      //需在用户可能点击分享按钮前就先调用
           wx.updateTimelineShareData({ 
               title: '赵峰&王一雪婚礼请柬', // 分享标题
-              desc: '邀请您和家人于10月27日，在北京饭店诺金参加赵峰和王一雪的婚礼，恭候您的光临。', // 分享描述
+              desc: '邀请您和家人于10月27日光临北京饭店参加赵峰和王一雪的婚礼。', // 分享描述
               link: 'https://weixin.youheng-inc.com/invitation/index.html?src=circle', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
               imgUrl: 'https://invitation.youheng-inc.com/images/o_1c53742l51g721o5d12lb1d5812dvqe.jpg', // 分享图标
               success: function () {
@@ -65,7 +65,35 @@ wx.ready(function() {
 wx.miniProgram.getEnv(function(res) {
   if (res.miniprogram) {
   	// 小程序中的逻辑
+
+   $(document).on('touchstart', '.navigation', function () {
+      if ($this.type) {
+        return false
+      }
+      wx.miniProgram.navigateTo({url: '/pages/map/map'})
+    })
+    $(document).on('touchstart', '.dwIcon', function () {
+      if ($this.type) {
+        return false
+      }
+      wx.miniProgram.navigateTo({url: '/pages/map/map'})
+    })
+
+
+
   } else {
   	// 网页的逻辑
+    $(document).on('touchstart', '.navigation', function () {
+      if ($this.type) {
+        return false
+      }
+      location.href = $this.mapUrl
+    })
+    $(document).on('touchstart', '.dwIcon', function () {
+      if ($this.type) {
+        return false
+      }
+      location.href = $this.mapUrl
+    })
   }
 })
